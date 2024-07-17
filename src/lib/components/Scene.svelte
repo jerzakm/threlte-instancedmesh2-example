5cd72ca5cf7d1c40bced573f1f01e0289ef14450
Marcin Jerzak <jerzakm@gmail.com>
2024-07-17 18:50:56 +0200

    threlte instancedmesh2

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,81 +1,32 @@
 <script lang="ts">
-  import { T } from '@threlte/core'
-  import { ContactShadows, Float, Grid, OrbitControls } from '@threlte/extras'
+  import { T, useThrelte } from "@threlte/core";
+  import { OrbitControls, Sky, useGltf } from "@threlte/extras";
+  import { FogExp2 } from "three";
+  import Trees from "./Trees.svelte";
+
+  const count = 1000000;
+  const terrainSize = 125000;
+
+  const gltf = useGltf("/tree.glb");
+
+  const { scene } = useThrelte();
+
+  scene.fog = new FogExp2("white", 0.0004);
 </script>
 
-<T.PerspectiveCamera
-  makeDefault
-  position={[-10, 10, 10]}
-  fov={15}
->
-  <OrbitControls
-    autoRotate
-    enableZoom={false}
-    enableDamping
-    autoRotateSpeed={0.5}
-    target.y={1.5}
-  />
+<T.PerspectiveCamera makeDefault position={[0, 100, 0]} fov={70} far={3000}>
+  <OrbitControls target.y={1.5} target.z={3} />
 </T.PerspectiveCamera>
 
-<T.DirectionalLight
-  intensity={0.8}
-  position.x={5}
-  position.y={10}
-/>
-<T.AmbientLight intensity={0.2} />
+<T.DirectionalLight intensity={0.8} position.x={5} position.y={10} />
 
-<Grid
-  position.y={-0.001}
-  cellColor="#ffffff"
-  sectionColor="#ffffff"
-  sectionThickness={0}
-  fadeDistance={25}
-  cellSize={2}
-/>
+<Sky scale={terrainSize} />
 
-<ContactShadows
-  scale={10}
-  blur={2}
-  far={2.5}
-  opacity={0.5}
-/>
+<T.Mesh receiveShadow rotation.x={Math.PI / -2}>
+  <T.PlaneGeometry args={[terrainSize, terrainSize, 10, 10]} />
+  <T.MeshLambertMaterial color={0x004622} />
+</T.Mesh>
 
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position.y={1.2}
-    position.z={-0.75}
-  >
-    <T.BoxGeometry />
-    <T.MeshStandardMaterial color="#0059BA" />
-  </T.Mesh>
-</Float>
-
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[1.2, 1.5, 0.75]}
-    rotation.x={5}
-    rotation.y={71}
-  >
-    <T.TorusKnotGeometry args={[0.5, 0.15, 100, 12, 2, 3]} />
-    <T.MeshStandardMaterial color="#F85122" />
-  </T.Mesh>
-</Float>
-
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[-1.4, 1.5, 0.75]}
-    rotation={[-5, 128, 10]}
-  >
-    <T.IcosahedronGeometry />
-    <T.MeshStandardMaterial color="#F8EBCE" />
-  </T.Mesh>
-</Float>
+{#if $gltf}
+  <Trees mesh={$gltf.scene.children[0]} {count} />
+{/if}
---
+++ src/lib/components/Trees.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { CullingBVH, InstancedMesh2 } from "@three.ez/instanced-mesh-test";
+  import { useThrelte } from "@threlte/core";
+  import { Mesh, BufferGeometry, MeshStandardMaterial } from "three";
+  import { T } from "@threlte/core";
+
+  export let mesh: Mesh<BufferGeometry, MeshStandardMaterial>;
+  export let count: number;
+
+  const terrainSize = 125000;
+
+  const { renderer } = useThrelte();
+
+  const trees = new InstancedMesh2(renderer, count, {
+    cullingType: CullingBVH,
+    geometry: mesh.geometry,
+    material: mesh.material,
+    onInstanceCreation: (obj, index) => {
+      obj.position
+        .setX(Math.random() * terrainSize - terrainSize / 2)
+        .setZ(Math.random() * terrainSize - terrainSize / 2);
+      obj.scale.setScalar(Math.random() * 0.1 + 0.1);
+      obj
+        .rotateY(Math.random() * Math.PI * 2)
+        .rotateZ(Math.random() * 0.3 - 0.15);
+    },
+  });
+
+  // trees.castShadow = true;
+  // trees.cursor = "pointer";
+
+  // trees.on("click", (e) => {
+  //   trees.instances[e.intersection.instanceId].visible = false;
+  // });
+</script>
+
+<T is={trees} />
